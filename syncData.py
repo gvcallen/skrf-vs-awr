2aef329c8d6b3dddf5816ad2645b4fc1feab4fa3
Gary Allen <gvcallen@gmail.com>
2024-11-07 12:03:08 +0200

    Updated sync script

--- syncData.py
+++ syncData.py
@@ -1,3 +1,11 @@
+# Personal script for gvcallen to sync external data from AWR folder locally into this repo
+import shutil
+import os
+
 path_simulated = '../../Data/Simulated/SMAMicrostripOpen'                   # path to simulated data
 path_measured = '../../Data/Measured/SMAMicrostripOpen'                     # path to measured data
+paths = [path_simulated, path_measured]
 
+shutil.rmtree('data')
+for path in paths:
+    shutil.copytree(path, 'data', dirs_exist_ok=True)
